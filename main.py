750a26346969a9902698ef648930da6ea0ff0952
sshoemaker@smu.edu <sshoemaker@smu.edu>
2021-12-03 15:12:06 -0600

    Add paper function complete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from pymongo import MongoClient
 
-from pprint import pprint
-
-from tkinter import *
-from tkinter import ttk
 
 client= MongoClient("mongodb://root:example@localhost:27017")
 
@@ -16,43 +12,83 @@ papers.create_index("publication.name")
 papers.create_index("publication.year")
 
 authors = db["authors"]
+authors.create_index("fname")
+authors.create_index("lname")
 papers.create_index("name")
 
-def addAuthor(entries):
-    try:
-        author = {}
-        for ent in entries:
-            print(ent)
-            author[ent] = entries[ent].get()
+def submitPaper(paper):
+    
+    if(paper["title"] and isinstance(paper["title"],str)):
+        if(papers.count_documents({"title": paper["title"]}) == 0):
+            authorIds= []
+            for author in paper["authors"]:
 
-            print(entries[ent].get())
-        authors.insert_one(author)
-    except ValueError:
-        pass
+                dbAuthors = authors.find( {"fname": author["fname"], "lname": author["lname"]})
+                
+                validAuthorFound = False
+                for dbAuthor in dbAuthors:
+
+                    for org in dbAuthor["organizations"]:
+                        
+                        if(not ((org["startYear"] < author["startYear"] and org["endYear"] > author["startYear"])
+                            or (author["startYear"] < org["startYear"] and author["startYear"]> org["endYear"]))):
+                            dbAuthor["organizations"].append(author["organization"])
+                            authorIds.append(dbAuthor["_id"])
+                            authors.replace_one({"_id":dbAuthor["_id"]}, dbAuthor)
+                            validAuthorFound = True
+                            break
+                    if(validAuthorFound):
+                        break
+                if not validAuthorFound:
+                    organizations = []
+                    
+                    organizations.append({"organization": author["organization"], "startYear": author["startYear"], "endYear" :author["endYear"]})
+
+                    author["organizations"] = organizations
+                    author.pop("organization")
+                    author.pop("startYear")
+                    author.pop("endYear")
+
+                    id = authors.insert_one(author).inserted_id
+
+                    authorIds.append(id)
+            paper["authors"] = authorIds
+            
+            
+            papers.insert_one(paper)
 
 
-def submitForm(root, fields,title):
-    entries = {}
-    for field in fields:
-        row = Frame(root)
-        lab = Label(row, width=22, text=field+": ", anchor='w')
-        ent = Entry(row)
-        
-        row.pack(side = TOP, fill = X, padx = 5 , pady = 5)
-        lab.pack(side = LEFT)
-        ent.pack(side = RIGHT, expand = YES, fill = X)
-        entries[field] = ent
+doc = { 
+    'title':"Test", 
+    'authors': [
+        {
+            "fname": "Bob",
+            "lname": "Bobbson",
+            "organization": "SMU",
+            "startYear": 1992,
+            "endYear": 1997
+        },
+        {
+            "fname": "Dave",
+            "lname": "Applebee",
+            "organization":"Applebees",
+            "startYear": 1992,
+            "endYear": 1995
+        }],
+    "publication": {
+        "name": "CS Journal",
+        "month": "January",
+        "year": 1994,
+        "type": "Journal"
+    }
+}
 
-    row = Frame(root)
-    row.pack(side = TOP, fill = X, padx = 5 , pady = 5)
-    Button(row, width=22, text=title,command=lambda e =entries: addAuthor(e)).pack(side="left")
-    return entries
+
+submitPaper(doc)
 
 
-authorFields=("First Name", "Last Name", "Organization", "Start Year", "Last Year")
 
-root = Tk()
-submitForm(root,authorFields,"Submit Author")
 
-root.mainloop()
+
+
 
